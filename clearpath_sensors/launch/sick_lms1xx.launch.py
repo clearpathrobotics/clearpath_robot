4bba31b17ef9b238d1794519afba22a3b9aa3cdc
Roni Kreinin <rkreinin@clearpathrobotics.com>
2023-04-27 14:27:23 -0400

    Licenses sick launch

--- clearpath_sensors/launch/sick_lms1xx.launch.py
+++ clearpath_sensors/launch/sick_lms1xx.launch.py
@@ -26,8 +26,7 @@
 # ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
-from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.actions import DeclareLaunchArgument
 from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
 
 from launch_ros.actions import Node
@@ -35,36 +34,31 @@ from launch_ros.substitutions import FindPackageShare
 
 
 def generate_launch_description():
-    pkg_microstrain_inertial_driver = FindPackageShare('microstrain_inertial_driver')
-
     parameters = LaunchConfiguration('parameters')
     namespace = LaunchConfiguration('namespace')
 
-    launch_microstrain_imu = PathJoinSubstitution([
-        pkg_microstrain_inertial_driver, 'launch', 'microstrain_launch.py'])
-
     arg_namespace = DeclareLaunchArgument(
         'namespace',
-        default_value='platform/sensors/imu_0')
+        default_value='platform/sensors/lidar2d_0')
 
     arg_parameters = DeclareLaunchArgument(
         'parameters',
         default_value=PathJoinSubstitution([
           FindPackageShare('clearpath_sensors'),
           'config',
-          'microstrain_imu.yaml'
+          'sick_lms1xx.yaml'
         ]))
 
-    launch_microstrain_imu = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource([launch_microstrain_imu]),
-        launch_arguments=[
-          ('namespace', namespace),
-          ('params_file', parameters)
-        ]
+    sick_scan_node = Node(
+        package='sick_scan',
+        namespace=namespace,
+        executable='sick_generic_caller',
+        arguments=[parameters],
+        output='screen',
     )
 
     ld = LaunchDescription()
     ld.add_action(arg_namespace)
     ld.add_action(arg_parameters)
-    ld.add_action(launch_microstrain_imu)
+    ld.add_action(sick_scan_node)
     return ld
