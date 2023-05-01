22358672f4f16c3bc07066cf9f07fb819caef20d
Roni Kreinin <rkreinin@clearpathrobotics.com>
2023-05-01 14:09:00 -0400

    Linter fix

--- clearpath_sensors/launch/microstrain_imu.launch.py
+++ clearpath_sensors/launch/microstrain_imu.launch.py
@@ -30,7 +30,6 @@ from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
 
-from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
 
 
