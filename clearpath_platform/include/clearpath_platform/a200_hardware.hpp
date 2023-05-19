d952f2a74d95ce1e192e18a066d5db53674ee52d
Tony Baltovski <tbaltovski@clearpathrobotics.com>
2023-05-19 10:18:05 -0400

    [clearpath_platform] Added A200 status topics.

--- clearpath_platform/include/clearpath_platform/a200_hardware.hpp
+++ clearpath_platform/include/clearpath_platform/a200_hardware.hpp
@@ -1,6 +1,7 @@
 #ifndef CLEARPATH_PLATFORM__A200_HARDWARE_HPP_
 #define CLEARPATH_PLATFORM__A200_HARDWARE_HPP_
 
+#include <chrono>
 #include <memory>
 #include <string>
 #include <vector>
@@ -12,11 +13,17 @@
 #include "hardware_interface/visibility_control.h"
 #include "rclcpp/macros.hpp"
 
-#include <chrono>
-
 #include "rclcpp/rclcpp.hpp"
 
+#include "std_msgs/msg/bool.hpp"
+#include "std_msgs/msg/float32.hpp"
+
 #include "clearpath_platform/horizon_legacy/horizon_legacy_wrapper.h"
+#include "clearpath_platform/a200_status.hpp"
+
+#include "clearpath_platform_msgs/msg/power.hpp"
+#include "clearpath_platform_msgs/msg/status.hpp"
+#include "clearpath_platform_msgs/msg/stop_status.hpp"
 
 using namespace std::chrono_literals;
 
@@ -56,6 +63,7 @@ private:
   void writeCommandsToHardware();
   void limitDifferentialSpeed(double &diff_speed_left, double &diff_speed_right);
   void updateJointsFromHardware();
+  void readStatusFromHardware();
   uint8_t isLeft(const std::string &str);
 
   // ROS Parameters
@@ -68,6 +76,14 @@ private:
   std::vector<double> hw_states_position_, hw_states_position_offset_, hw_states_velocity_;
 
   uint8_t left_cmd_joint_index_, right_cmd_joint_index_;
+
+  std::shared_ptr<a200_status::A200Status> status_node_;
+  clearpath_platform_msgs::msg::Power power_msg_;
+  clearpath_platform_msgs::msg::Status status_msg_;
+  clearpath_platform_msgs::msg::StopStatus stop_status_msg_;
+  std_msgs::msg::Bool stop_msg_;
+  std_msgs::msg::Float32 driver_left_temp_msg_, driver_right_temp_msg_;
+  std_msgs::msg::Float32 motor_left_temp_msg_, motor_right_temp_msg_;
 };
 
 }  // namespace clearpath_platform
