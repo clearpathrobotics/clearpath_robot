3f8b855e95253177d220517a1eeffce0b4652744
Roni Kreinin <rkreinin@clearpathrobotics.com>
2023-10-12 14:57:24 -0400

    rolling average

--- clearpath_diagnostics/clearpath_diagnostics/battery_state_publisher/battery.py
+++ clearpath_diagnostics/clearpath_diagnostics/battery_state_publisher/battery.py
@@ -32,72 +32,70 @@
 # modification, is not permitted without the express permission
 # of Clearpath Robotics.
 
-from builtin_interfaces.msg import Time
+from clearpath_platform_msgs.msg import Power
 from sensor_msgs.msg import BatteryState
-from enum import Enum
 
 
 class Battery:
-    class Type(Enum):
-        HE2613 = 0
-
-    class Calculation(Enum):
-        LINEAR_INTERPOLATION = 0
-        BMS = 1
-
-    # Lookup table
-    LUT = {
-        Type.HE2613: [
-            [21.00, 0.0],
-            [21.84, 0.1],
-            [22.68, 0.2],
-            [23.52, 0.3],
-            [24.36, 0.4],
-            [25.20, 0.5],
-            [26.04, 0.6],
-            [26.88, 0.7],
-            [27.72, 0.8],
-            [28.56, 0.9],
-            [29.40, 1.0],
-        ]
-    }
-
-    def __init__(self, battery_type: Type) -> None:
-        self.battery_type = battery_type
+    def __init__(self, rolling_average_period=30) -> None:
         self._msg = BatteryState()
-        self._msg.header.frame_id = 'battery_link'
-
-        match self.battery_type:
-            case Battery.Type.HE2613:
-                self.calculation = Battery.Calculation.LINEAR_INTERPOLATION
-                self.lut = Battery.LUT[Battery.Type.HE2613]
-                self._msg.power_supply_technology = BatteryState.POWER_SUPPLY_TECHNOLOGY_LION
+        self._msg.header.frame_id = "battery_link"
+        self._msg.power_supply_technology = BatteryState.POWER_SUPPLY_TECHNOLOGY_UNKNOWN
+        self._readings: list[Power] = []
+        self.rolling_average_period = rolling_average_period
 
     @property
     def msg(self) -> BatteryState:
         return self._msg
 
-    def update(self, voltage: float, current: float):
-        self._msg.voltage = voltage
-        self._msg.current = current
-        match self.calculation:
-            case Battery.Calculation.LINEAR_INTERPOLATION:
-                self._msg.percentage = self.linear_extrapolation(self.lut, voltage)
-            case Battery.Calculation.BMS:
-                pass
+    def update(self, power_msg: Power):
+        # Add new reading to rolling average
+        self._readings.append(power_msg)
+        self._msg.header = power_msg.header
+        # Remove oldest reading
+        if len(self._readings) > self.rolling_average_period:
+            self._readings.pop(0)
 
-    def stamp(self, stamp: Time):
-        self._msg.header.stamp = stamp
-
-    def linear_extrapolation(self, lut: list[list], v: float) -> float:
+    def linear_interpolation(self, lut: list[list], v: float) -> float:
         # Check if voltage is below minimum value
         if v <= lut[0][0]:
             return lut[0][1]
 
         for i in range(0, len(lut)):
             if v < lut[i][0]:
-                return (v - lut[i - 1][0]) * (lut[i][1] - lut[i - 1][1]) / \
-                       (lut[i][0] - lut[i - 1][0]) + lut[i - 1][1]
+                return (v - lut[i - 1][0]) * (lut[i][1] - lut[i - 1][1]) / (
+                    lut[i][0] - lut[i - 1][0]) + lut[i - 1][1]
 
         # Return maximum value
         return lut[len(lut) - 1][1]
+
+
+class HE2613(Battery):
+    LUT = [
+        [21.00, 0.0],
+        [21.84, 0.1],
+        [22.68, 0.2],
+        [23.52, 0.3],
+        [24.36, 0.4],
+        [25.20, 0.5],
+        [26.04, 0.6],
+        [26.88, 0.7],
+        [27.72, 0.8],
+        [28.56, 0.9],
+        [29.40, 1.0],
+    ]
+
+    def __init__(self) -> None:
+        super().__init__()
+        self._msg.power_supply_technology = BatteryState.POWER_SUPPLY_TECHNOLOGY_LION
+
+    def update(self, power_msg: Power):
+        super().update(power_msg)
+        self._msg.voltage = power_msg.measured_voltages[Power.JACKAL_MEASURED_BATTERY]
+        self._msg.current = power_msg.measured_currents[Power.JACKAL_TOTAL_CURRENT]
+
+        avg_voltage = 0
+        for reading in self._readings:
+            avg_voltage += reading.measured_voltages[Power.JACKAL_MEASURED_BATTERY]
+        avg_voltage /= len(self._readings)
+        self._msg.percentage = self.linear_interpolation(self.LUT, avg_voltage)
