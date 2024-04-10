01605a820aa228c522edaa362652ed20ffd4bd43
Luis Camero <lcamero@clearpathrobotics.com>
2024-04-10 13:49:50 -0400

    Added pytest to check config

---
+++ clearpath_generator_robot/test/test_generator_param.py
@@ -0,0 +1,55 @@
+# Software License Agreement (BSD)
+#
+# @author    Luis Camero <lcamero@clearpathrobotics.com>
+# @copyright (c) 2024, Clearpath Robotics, Inc., All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+# * Redistributions of source code must retain the above copyright notice,
+#   this list of conditions and the following disclaimer.
+# * Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+# * Neither the name of Clearpath Robotics nor the names of its contributors
+#   may be used to endorse or promote products derived from this software
+#   without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+from clearpath_generator_robot.param.generator import RobotParamGenerator
+import os
+import shutil
+
+SAMPLE_DIR = "/opt/ros/humble/share/clearpath_config/sample/"
+
+
+class TestRobotLaunchGenerator:
+
+    def test_samples(self):
+        errors = []
+        for sample in os.listdir(SAMPLE_DIR):
+            # Create Clearpath Directory
+            src = os.path.join(SAMPLE_DIR, sample)
+            dst = os.path.join(os.environ["HOME"], ".clearpath", "robot.yaml")
+            shutil.rmtree(os.path.dirname(dst))
+            os.makedirs(os.path.dirname(dst), exist_ok=True)
+            shutil.copy(src, dst)
+            # Generate
+            try:
+                rpg = RobotParamGenerator(os.path.dirname(dst))
+                rpg.generate()
+            except Exception as e:
+                errors.append("Sample '%s' failed to load: '%s'" % (
+                    sample,
+                    e.args[0],
+                ))
+        assert not errors, "Errors: %s" % "\n".join(errors)
