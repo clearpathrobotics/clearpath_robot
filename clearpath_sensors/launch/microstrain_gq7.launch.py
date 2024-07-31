48bfddc70212e071af4c8617cf877745d42320bf
robbiefish <rob.fisher@hbkworld.com>
2024-07-31 20:10:27 +0000

    Adds GQ7 yaml and launch file

---
+++ clearpath_sensors/launch/microstrain_gq7.launch.py
@@ -0,0 +1,83 @@
+# Software License Agreement (BSD)
+#
+# @author    Roni Kreinin <rkreinin@clearpathrobotics.com>
+# @copyright (c) 2023, Clearpath Robotics, Inc., All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+# * Redistributions of source code must retain the above copyright notice,
+#   this list of conditions and the following disclaimer.
+# * Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+# * Neither the name of Clearpath Robotics nor the names of its contributors
+#   may be used to endorse or promote products derived from this software
+#   without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, GroupAction, IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+
+from launch_ros.actions import SetRemap
+from launch_ros.substitutions import FindPackageShare
+
+
+def generate_launch_description():
+    pkg_microstrain_inertial_driver = FindPackageShare('microstrain_inertial_driver')
+
+    parameters = LaunchConfiguration('parameters')
+    namespace = LaunchConfiguration('namespace')
+    robot_namespace = LaunchConfiguration('robot_namespace')
+
+    launch_microstrain_imu = PathJoinSubstitution([
+        pkg_microstrain_inertial_driver, 'launch', 'microstrain_launch.py'])
+
+    arg_namespace = DeclareLaunchArgument(
+        'namespace',
+        default_value='')
+
+    arg_robot_namespace = DeclareLaunchArgument(
+        'robot_namespace',
+        default_value='')
+
+    arg_parameters = DeclareLaunchArgument(
+        'parameters',
+        default_value=PathJoinSubstitution([
+          FindPackageShare('clearpath_sensors'),
+          'config',
+          'microstrain_gq7.yaml'
+        ]))
+
+    launch_microstrain_imu = GroupAction([
+        SetRemap('/tf', 'tf'),
+        SetRemap('/tf_static', PathJoinSubstitution(['/', robot_namespace, 'tf_static'])),
+
+        IncludeLaunchDescription(
+          PythonLaunchDescriptionSource([launch_microstrain_imu]),
+          launch_arguments=[
+            ('namespace', namespace),
+            ('params_file', parameters),
+            ('configure', 'true'),
+            ('activate', 'true')
+          ]
+        )
+    ])
+
+    ld = LaunchDescription()
+    ld.add_action(arg_namespace)
+    ld.add_action(arg_robot_namespace)
+    ld.add_action(arg_parameters)
+    ld.add_action(launch_microstrain_imu)
+    return ld
