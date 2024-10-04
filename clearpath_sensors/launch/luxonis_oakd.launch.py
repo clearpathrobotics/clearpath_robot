4421c57253042f08825181ffde13db2e6d53727d
luis-camero <88782189+luis-camero@users.noreply.github.com>
2024-10-04 11:56:55 -0400

    Add OAKD (#92)
    * Add OAKD
    
    * Remove unused parameter
    
    * Add UDEV for OAKD

---
+++ clearpath_sensors/launch/luxonis_oakd.launch.py
@@ -0,0 +1,99 @@
+# Software License Agreement (BSD)
+#
+# @author    Luis Camero <lcamero@clearpathrobotics.com>
+# @copyright (c) 2024, Clearpath Robotics, Inc., All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+# * Redistributions of source code must retain the above copyright notice,
+#   this list of conditions and the following disclaimer.
+# * Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+# * Neither the name of Clearpath Robotics nor the names of its contributors
+#   may be used to endorse or promote products derived from this software
+#   without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+
+from launch_ros.actions import ComposableNodeContainer
+from launch_ros.descriptions import ComposableNode
+from launch_ros.substitutions import FindPackageShare
+
+
+def generate_launch_description():
+    parameters = LaunchConfiguration('parameters')
+    namespace = LaunchConfiguration('namespace')
+
+    arg_parameters = DeclareLaunchArgument(
+        'parameters',
+        default_value=PathJoinSubstitution([
+          FindPackageShare('clearpath_sensors'),
+          'config',
+          'intel_realsense.yaml'
+        ]))
+
+    arg_namespace = DeclareLaunchArgument(
+        'namespace',
+        default_value='')
+
+    depthai_oakd_node = ComposableNode(
+        package='depthai_ros_driver',
+        name='oakd',
+        namespace=namespace,
+        plugin='depthai_ros_driver::Camera',
+        parameters=[parameters],
+        remappings=[
+            ('~/imu/data', 'imu/data'),
+            ('~/nn/spatial_detections', 'nn/spatial_detections'),
+            ('~/rgb/camera_info', 'color/camera_info'),
+            ('~/rgb/image_raw', 'color/image'),
+            ('~/rgb/image_raw/compressed', 'color/compressed'),
+            ('~/rgb/image_raw/compressedDepth', 'color/compressedDepth'),
+            ('~/rgb/image_raw/ffmpeg', 'color/ffmpeg'),
+            ('~/rgb/image_raw/theora', 'color/theora'),
+            ('~/rgb/preview/image_raw', 'color/image'),
+            ('~/rgb/preview/image_raw/compressed', 'color/compressed'),
+            ('~/rgb/preview/image_raw/compressedDepth', 'color/compressedDepth'),
+            ('~/rgb/preview/image_raw/ffmpeg', 'color/ffmpeg'),
+            ('~/rgb/preview/image_raw/theora', 'color/theora'),
+            ('~/stereo/camera_info', 'stereo/camera_info'),
+            ('~/stereo/image_raw', 'stereo/image'),
+            ('~/stereo/image_raw/compressed', 'stereo/compressed'),
+            ('~/stereo/image_raw/compressedDepth', 'stereo/compressedDepth'),
+            ('~/stereo/image_raw/ffmpeg', 'stereo/ffmpeg'),
+            ('~/stereo/image_raw/theora', 'stereo/theora'),
+            ('/diagnostics', 'diagnostics'),
+        ],
+        extra_arguments=[{'use_intra_process_comms': True}],
+    )
+
+    image_processing_container = ComposableNodeContainer(
+        name='image_processing_container',
+        namespace=namespace,
+        package='rclcpp_components',
+        executable='component_container',
+        composable_node_descriptions=[
+          depthai_oakd_node
+        ],
+        output='screen'
+    )
+
+    ld = LaunchDescription()
+    ld.add_action(arg_parameters)
+    ld.add_action(arg_namespace)
+    ld.add_action(image_processing_container)
+    return ld
