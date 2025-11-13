bed7710259f93b105fc1f57f91da3c5eb54123d5
Tony Baltovski <tbaltovski@clearpathrobotics.com>
2025-11-13 12:59:09 -0500

    2.8.4

--- clearpath_tests/setup.py
+++ clearpath_tests/setup.py
@@ -35,7 +35,7 @@ package_name = 'clearpath_tests'
 
 setup(
     name=package_name,
-    version='2.8.3',
+    version='2.8.4',
     packages=find_packages(exclude=['test']),
     data_files=[
         ('share/ament_index/resource_index/packages',
