cc6b2c7d8a85c9a87ab62c23ba6524e4146b13cc
Tony Baltovski <tbaltovski@clearpathrobotics.com>
2026-04-14 21:10:10 -0400

    Fix OAK-D camera launch, node naming, and point cloud output (#327)
    (cherry picked from commit 4e350fbd481890ea70ffe70d5c627c790e863438)
    
    # Conflicts:
    #	clearpath_sensors/launch/luxonis_oakd.launch.py

--- clearpath_sensors/launch/luxonis_oakd.launch.py
+++ clearpath_sensors/launch/luxonis_oakd.launch.py
@@ -25,6 +25,7 @@
 # CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
 # ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
+
 import os
 
 from launch import LaunchDescription
@@ -34,7 +35,50 @@ from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
 from launch_ros.actions import ComposableNodeContainer
 from launch_ros.descriptions import ComposableNode
 from launch_ros.substitutions import FindPackageShare
+import yaml
 
+# Pipeline types that produce stereo/depth output
+DEPTH_PIPELINES = ['RGBD', 'RGBStereo', 'Stereo', 'Depth', 'DepthToF', 'StereoToF']
+
+CAMERAS = [
+    'rgb',
+    'stereo',
+]
+
+CAMERA_RENAMES = {
+    'rgb': 'color',
+    'stereo': 'stereo',
+}
+
+IMAGES = [
+    'image_raw',
+]
+
+TOPICS = [
+    'camera_info',
+]
+
+PREVIEWS = [
+    'rgb',
+]
+
+<<<<<<< HEAD
+=======
+TRANSPORTS = [
+    'compressed',
+    'compressedDepth',
+    'theora',
+    'ffmpeg',
+    'zstd',
+    'foxglove',
+]
+
+OTHERS = [
+    'imu/data',
+    'nn/spatial_detections',
+]
+
+>>>>>>> 4e350fb (Fix OAK-D camera launch, node naming, and point cloud output (#327))
 
 def launch_setup(context):
     parameters = LaunchConfiguration('parameters')
@@ -42,12 +86,73 @@ def launch_setup(context):
 
     name = os.path.basename(namespace.perform(context))
 
+<<<<<<< HEAD
+=======
+    # Determine pipeline type from parameters file
+    params_file = parameters.perform(context)
+    pipeline_type = 'RGBD'
+    try:
+        with open(params_file, 'r') as f:
+            params = yaml.safe_load(f)
+        for key, val in params.items():
+            if isinstance(val, dict):
+                ros_params = val.get('ros__parameters', val)
+                # Check nested camera.i_pipeline_type
+                camera = ros_params.get('camera', {})
+                if isinstance(camera, dict) and 'i_pipeline_type' in camera:
+                    pipeline_type = camera['i_pipeline_type']
+                # Check flattened camera.i_pipeline_type
+                if 'camera.i_pipeline_type' in ros_params:
+                    pipeline_type = ros_params['camera.i_pipeline_type']
+    except Exception:
+        pass
+
+    remappings = [
+        ('/diagnostics', 'diagnostics'),
+    ]
+
+    for camera in CAMERAS:
+        renamed = CAMERA_RENAMES[camera]
+        for image in IMAGES:
+            remappings.append(
+                (f'~/{camera}/{image}', f'{renamed}/image'),
+            )
+            for transport in TRANSPORTS:
+                remappings.append(
+                    (f'~/{camera}/{image}/{transport}', f'{renamed}/{transport}'),
+                )
+        for topic in TOPICS:
+            remappings.append(
+                (f'~/{camera}/{topic}', f'{renamed}/{topic}'),
+            )
+
+    for camera in PREVIEWS:
+        renamed = CAMERA_RENAMES[camera]
+        for image in IMAGES:
+            remappings.append(
+                (f'~/{camera}/preview/{image}', f'{renamed}/preview/image'),
+            )
+            for transport in TRANSPORTS:
+                remappings.append(
+                    (f'~/{camera}/preview/{image}/{transport}',
+                     f'{renamed}/preview/{transport}'),
+                )
+        for topic in TOPICS:
+            remappings.append(
+                (f'~/{camera}/preview/{topic}', f'{renamed}/preview/{topic}'),
+            )
+
+    for topic in OTHERS:
+        remappings.append(('~/%s' % topic, '%s' % topic))
+
+>>>>>>> 4e350fb (Fix OAK-D camera launch, node naming, and point cloud output (#327))
     depthai_oakd_node = ComposableNode(
         package='depthai_ros_driver',
         name=name,
         namespace=namespace,
         plugin='depthai_ros_driver::Camera',
         parameters=[parameters],
+<<<<<<< HEAD
         remappings=[
             ('~/imu/data', 'imu/data'),
             ('~/nn/spatial_detections', 'nn/spatial_detections'),
@@ -71,28 +176,33 @@ def launch_setup(context):
             ('/diagnostics', 'diagnostics'),
         ],
         extra_arguments=[{'use_intra_process_comms': True}],
+=======
+        remappings=remappings,
+>>>>>>> 4e350fb (Fix OAK-D camera launch, node naming, and point cloud output (#327))
     )
 
-    depthai_pcl_node = ComposableNode(
-        package='depth_image_proc',
-        plugin='depth_image_proc::PointCloudXyzNode',
-        name='point_cloud_xyz_node',
-        namespace=namespace,
-        remappings=[
-            ('image_rect', 'stereo/image'),
-            ('camera_info', 'stereo/camera_info'),
-        ],
-    )
+    composable_nodes = [depthai_oakd_node]
+
+    if pipeline_type in DEPTH_PIPELINES:
+        depthai_pcl_node = ComposableNode(
+            package='depth_image_proc',
+            plugin='depth_image_proc::PointCloudXyzrgbNode',
+            name='point_cloud_xyzrgb_node',
+            namespace=namespace,
+            remappings=[
+                ('depth_registered/image_rect', 'stereo/image'),
+                ('rgb/image_rect_color', 'color/image'),
+                ('rgb/camera_info', 'color/camera_info'),
+            ],
+        )
+        composable_nodes.append(depthai_pcl_node)
 
     image_processing_container = ComposableNodeContainer(
         name='image_processing_container',
         namespace=namespace,
         package='rclcpp_components',
-        executable='component_container',
-        composable_node_descriptions=[
-          depthai_oakd_node,
-          depthai_pcl_node,
-        ],
+        executable='component_container_mt',
+        composable_node_descriptions=composable_nodes,
         output='screen'
     )
 
